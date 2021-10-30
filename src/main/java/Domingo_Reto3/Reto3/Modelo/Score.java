7b7b130af5210fd12033c04f89653392181548dc
maria campos <ivonnecampos2014@gmail.com>
2021-10-29 22:06:00 -0500

    first commit

---
+++ src/main/java/Domingo_Reto3/Reto3/Modelo/Score.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domingo_Reto3.Reto3.Modelo;
+
+/**
+ *
+ * @author HP
+ */
+public class Score {
+    
+}
